62e7328d5e8e6b2e1eefe69fb96a1b348b7e5d0d
maxniederer <66148024+maxniederer@users.noreply.github.com>
2024-09-11 23:43:19 -0400

    adding sun/moon theme toggle symbol

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,30 @@
-// immediately invoked function expression (IIFE) pattern
-(function(){
-  var currTheme = localStorage.getItem('theme');
-  if (currTheme != 'dark' && currTheme != 'light') {
-    currTheme = 'dark';
+const darkModeIcon = document.getElementById("dark-mode-icon");
+
+(() => {
+  var currTheme = localStorage.getItem("theme");
+  if (currTheme != "dark" && currTheme != "light") {
+    currTheme = "light";
   }
-  document.documentElement.setAttribute('data-theme', currTheme);
+  document.documentElement.setAttribute("data-theme", currTheme);
+  toggleDarkModeIcon(currTheme);
 })();
 
 function toggleDarkMode() {
-  let targetTheme = document.documentElement.getAttribute('data-theme') == 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', targetTheme);
-  localStorage.setItem('theme', targetTheme);
+  let targetTheme =
+    document.documentElement.getAttribute("data-theme") == "dark"
+      ? "light"
+      : "dark";
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+  toggleDarkModeIcon(targetTheme);
+}
+
+function toggleDarkModeIcon(newTheme) {
+  if (newTheme == "dark") {
+    darkModeIcon.classList.add("fa-sun");
+    darkModeIcon.classList.remove("fa-moon");
+  } else if (newTheme == "light") {
+    darkModeIcon.classList.add("fa-moon");
+    darkModeIcon.classList.remove("fa-sun");
+  }
 }
